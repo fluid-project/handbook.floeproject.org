6040db63bbbba6d70004991c2f27457688bffa58
Ned Zimmerman <ned@bight.dev>
2020-10-26 14:17:47 -0300

    docs: remove file header info from Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,3 @@
-/*
-Copyright the Inclusive Learning Design Handbook copyright holders.
-
-See the AUTHORS.md file at the top-level directory of this distribution and at
-https://github.com/fluid-project/docs-inclusive-learning/raw/main/AUTHORS.md.
-
-Licensed under the New BSD license. You may not use this file except in compliance with this License.
-
-You may obtain a copy of the New BSD License at
-https://github.com/fluid-project/docs-inclusive-learning/raw/main/LICENSE.md.
-*/
-
 /* eslint-env node */
 "use strict";
 
