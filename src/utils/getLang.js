c5685641d7dd5601e48d45b6c33ebbccb9ccec52
BlueSlug <mossmonkey@gmail.com>
2022-02-04 01:56:29 -0500

    fix: solve bug with getLang function

--- src/utils/getLang.js
+++ src/utils/getLang.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports = (filePathStem, collection) => {
-    return filePathStem.replace(new RegExp(`\/collections\/${collection}\/([A-Za-z-]*)\/[a-z0-9-]*`, "g"), "$1");
+    return filePathStem.replace(new RegExp(`\/collections\/${collection}\/([A-Za-z-]*)\/[A-Za-z0-9-/]*`, "g"), "$1");
 };
