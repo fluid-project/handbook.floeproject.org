b699885a1b3484ff876f29c6f5f4c7a887fc69c5
BlueSlug <mossmonkey@gmail.com>
2022-02-24 12:02:54 -0500

    fix: solve missing site name in topic page titles

--- src/collections/perspectives/perspectives.11tydata.js
+++ src/collections/perspectives/perspectives.11tydata.js
@@ -1,9 +1,24 @@
 "use strict";
 
-const generateEleventyComputed = require("../../utils/generateEleventyComputed.js");
+const generatePermalink = require("../../utils/generatePermalink.js");
+const getLang = require("../../utils/getLang.js");
 
 module.exports = {
-    "layout": "layouts/topic-page",
-    "permalink": "perspectives/{{ title | slugify }}/",
-    eleventyComputed: data => generateEleventyComputed(data, "perspectives")
+    layout: "layouts/topic-page",
+    eleventyComputed: {
+        /* Determine the language of this item based on the language code in the file path. */
+        lang: data => getLang(data.page.filePathStem, "perspectives"),
+        /* Set the translationKey, used for populating the language switcher, to the file slug. */
+        translationKey: data => {
+            const lang = getLang(data.page.filePathStem, "perspectives");
+
+            if (data.page.fileSlug === lang) {
+                return "index";
+            }
+
+            return data.page.fileSlug;
+        },
+        /* Build a permalink using the post title, language key, and translated collection type slug. */
+        permalink: data => generatePermalink(data, "perspectives")
+    }
 };
--- src/collections/techniques/techniques.11tydata.js
+++ src/collections/techniques/techniques.11tydata.js
@@ -1,9 +1,24 @@
 "use strict";
 
-const generateEleventyComputed = require("../../utils/generateEleventyComputed.js");
+const generatePermalink = require("../../utils/generatePermalink.js");
+const getLang = require("../../utils/getLang.js");
 
 module.exports = {
-    "layout": "layouts/topic-page",
-    "permalink": "techniques/{{ title | slugify }}/",
-    eleventyComputed: data => generateEleventyComputed(data, "techniques")
+    layout: "layouts/topic-page",
+    eleventyComputed: {
+        /* Determine the language of this item based on the language code in the file path. */
+        lang: data => getLang(data.page.filePathStem, "techniques"),
+        /* Set the translationKey, used for populating the language switcher, to the file slug. */
+        translationKey: data => {
+            const lang = getLang(data.page.filePathStem, "techniques");
+
+            if (data.page.fileSlug === lang) {
+                return "index";
+            }
+
+            return data.page.fileSlug;
+        },
+        /* Build a permalink using the post title, language key, and translated collection type slug. */
+        permalink: data => generatePermalink(data, "techniques")
+    }
 };
